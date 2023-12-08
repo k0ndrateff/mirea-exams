dcbfced7eb1240a6c4923b12b6ecfe09c0e94c15
k0ndrateff <63606405+k0ndrateff@users.noreply.github.com>
2023-12-09 00:40:55 +0300

    🛠 add latex support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
 import { defineConfig } from 'astro/config';
 
+import mdx from "@astrojs/mdx";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [mdx({
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex]
+  })]
+});

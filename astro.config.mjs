64cf6424703d37ea830066e4966da1939de544a5
k0ndrateff <63606405+k0ndrateff@users.noreply.github.com>
2023-12-09 23:46:31 +0300

    🎨 add theory layout + basic text styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,18 @@ import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
+import typograf from "astro-typograf";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx({
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeKatex]
+  }),
+  typograf({
+    selector: 'p, h1, h2, h3, h4, li',
+    typografOptions: {
+      locale: ['ru', 'en-US']
+    }
   })]
 });

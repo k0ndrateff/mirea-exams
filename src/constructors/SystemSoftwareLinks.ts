d6621f74515ed6c39a85a530a741959a906801a6
Egor Kondratev <kondratevegor04@gmail.com>
2024-05-18 15:18:01 +0300

    📖 fill system software 31-44 + add practice block

---
+++ src/constructors/SystemSoftwareLinks.ts
@@ -0,0 +1,4 @@
+export const systemSoftwareLinks = (activeIdx: number) => [
+  { href: '/mirea-exams/system-software/theory', title: 'Знать', active: activeIdx === 0 },
+  { href:'/mirea-exams/system-software/practice', title:'Уметь', active: activeIdx === 1 }
+];
